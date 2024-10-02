62cd1bb8cb51c6233d0ea9e58d8affa2889ee9d6
Nithya2209balu <nithyabalu2209@gmail.com>
2024-10-02 19:04:42 +0530

    update  portfolio

---
+++ script.js
@@ -0,0 +1,20 @@
+let display = document.getElementById('display');
+
+function appendToDisplay(value) {
+if (display.textContent === '0' && value !== '.') {
+display.textContent = '';
+}
+display.textContent += value;
+}
+
+function clearDisplay() {
+display.textContent = '0';
+}
+
+function calculate() {
+try {
+display.textContent = eval(display.textContent);
+} catch (error) {
+display.textContent = 'Error';
+}
+}
